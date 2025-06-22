d17930aef650f8de3c02dfce7c50b920b4d7843c
wsk2121 <wansuk2549@gmail.com>
2025-06-22 05:24:59 +0000

    add password

---
+++ cell02/ex02/password.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+#Cell02Ex02 Password
+my_password = "Python is awesome"
+print ("Enter your password")
+password = input()
+if password == my_password:
+    print("ACCESS GRANTED")
+else:
+    print("ACCESS DENIED")    
