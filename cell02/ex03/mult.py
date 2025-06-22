6653f55c82683a64cc98f8ed6e472c1b1c30979a
wsk2121 <wansuk2549@gmail.com>
2025-06-22 05:25:47 +0000

    add mult

---
+++ cell02/ex03/mult.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+n1 = int(input("type a number: "))
+n2 = int(input("type a number: "))
+x = 0
+a = int(n1)
+b = int(n2)
+result = int(n1* n2)
+print(result)
+if result > x:
+    print("The result is positive")
+if result < x:
+    print("The result is negative")
+if result == x:
+    print("The result is positive and negative")        
