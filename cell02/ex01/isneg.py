d1e76f7f70d08ed2ae9634dbb31213dde7d28b2c
wsk2121 <wansuk2549@gmail.com>
2025-06-22 05:23:49 +0000

    add neg

---
+++ cell02/ex01/isneg.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+#Cell02Ex01 Am I negative
+number = int(input("Enter a number:"))
+if number <0:
+    print("The number is negative.")
+if number >0:
+    print("The number is positive.") 
+if number == 0:
+    print("The number is both positive and negative.")       
