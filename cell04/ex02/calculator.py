d6af3ccc5feea0469d3e0ad982a07703ff236e35
wsk2121 <wansuk2549@gmail.com>
2025-06-23 03:23:47 +0000

    add calc

---
+++ cell04/ex02/calculator.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+num1 = int(input("Give me the first number: "))
+num2 = int(input("Give me the second number: "))
+print("Thank you!")
+add_result = num1 + num2
+sub_result = num1 - num2
+div_result = num1 / num2
+mul_result = num1 * num2
+print(f"{num1} + {num2} = {add_result}")
+print(f"{num1} - {num2} = {sub_result}")
+print(f"{num1} / {num2} = {div_result}")
+print(f"{num1} * {num2} = {mul_result}")
